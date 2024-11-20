bffd56dd376a5b8454c1b0c253cc0153474d941a
Nazmul Hoque <sujon686500@gmail.com>
2024-11-20 15:52:28 +0600

    expressjs boilerplate

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.database_url}`);
+    });
+  } catch (err) {
+    console.log('error', err);
+  }
+}
